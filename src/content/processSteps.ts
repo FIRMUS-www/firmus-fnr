f62891f3d1e87c59896f131330a1055f01aebbed
FIRMUS-www <spolkafirmus@gmail.com>
2026-09-03 21:58:39 +0200

    Update processSteps.ts

--- src/content/processSteps.ts
+++ src/content/processSteps.ts
@@ -1,7 +1,7 @@
 export const steps: { n: string; t: string; d: string }[] = [
     { n: "01", t: "Wycena", d: "Całkowity miesięczny koszt od 55zł. Warunek: do 10 dokumentów + zapłata abonamentu na całyrok z góry = 660 zł. Inne scenariusze? - kliknij przycisk poniżej." },
     { n: "02", t: "Umowa", d: "Transparentna, prosta, bez drobnego druczku i ukrytych dopłat. Prosta do rozwiązania. Elastyczna - dopasowana do etapu na jakim jesteś." },
-    { n: "03", t: "Start", d: "Dostajesz apkę do wszystkiego: faktury, VAT, KSeF, ZUS itd. itp. wraz z łatwym dostępem do księgowego." },
+    { n: "03", t: "Apka", d: "W cenie masz apkę do wszystkiego: faktury, VAT, KSeF, ZUS itd. itp. wraz z łatwym dostępem do księgowego." },
     { n: "04", t: "Test", d: "Masz 30 dni na sprawdzenie. Nie pasuje? Kasa wraca do Ciebie. Bez pytań i bez problemów." },
     { n: "05", t: "Lecimy", d: "Zapominasz o pamiętaniu o terminach, limitach, formularzach i deklaracjach. Od tego jest księgowość." },
   ];
